3c408bb21bef87f0eab5ff9faa9ef1308830773b
Alksalt <ogano0296@gmail.com>
2025-08-01 13:26:17 +0200

    deleted

--- python-coding-challenges/leetcode_problems/LengthLastWord.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        return len(s.split()[-1])
-
-
-s = 'Fuck you idiot   ps sorry'
-sol = Solution()
-print(sol.lengthOfLastWord(s))
--- python-coding-challenges/leetcode_problems/lengthOfLastWord.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        return len(s.strip().split()[-1])
-
-s = "   fly me   to   the moon  "
-s2 = "luffy is still joyboy"
-sol = Solution()
-print(sol.lengthOfLastWord(s2))
