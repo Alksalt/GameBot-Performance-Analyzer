5334dc9beec3f5974ed5b6d003056c5208383e77
Oleksandr Altukhov <ogano0296@gmail.com>
2025-08-11 13:27:56 +0200

    new files

---
+++ src/true_false/__init__.py
@@ -0,0 +1,17 @@
+from .game import (
+    TrueFalseGame,
+    LocalTrueFalseGame,
+    GameConfig,
+    DATA_DIR,
+    SUMMARY_CSV,
+)
+
+__all__ = [
+    "TrueFalseGame",
+    "LocalTrueFalseGame",
+    "GameConfig",
+    "DATA_DIR",
+    "SUMMARY_CSV",
+]
+
+__version__ = "0.1.0"
